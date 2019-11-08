dcde599d7b047acb7e258b4463d9f36d84aa5a99
naser01 <nnkilani@live.com>
2019-11-08 18:39:29 +0200

    8

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,1 +1,23 @@
+import Search from './models/Search';
+import searchView from './views/searchView';
 
+
+const state = {};
+
+/** 
+ * SEARCH CONTROLLER
+ */
+
+const controlSearch = async() => {
+    //1)Get query from view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+        console.log(state.search);
+        console.log(new Search('london'));
+        
+        
+    }
+}
