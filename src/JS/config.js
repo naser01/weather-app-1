9adb5f0fad5cdf52d1a0adcd05c07d17521a8289
naser01 <nnkilani@live.com>
2019-11-08 18:05:55 +0200

    7

---
+++ src/JS/config.js
@@ -0,0 +1,2 @@
+export const key = '48e4bbf01e7d9297f784d4b9474efe79';
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
--- src/JS/models/Search.js
+++ src/JS/models/Search.js
@@ -1,29 +1,21 @@
-/*import axios from 'axios';
-
-export default class Search {
-    constructor(name) {
-        this.name = name;
-    }
-
-    async getResults() {
-        try {
-            const res = await axios(`https://www.metaweather.com//api/location/search/?query=(query)${this.name}`)
-        } catch (error) {
-            alert(error);
-        }
-    }
-};*/
+import {key, proxy} from '../config';
 
 import axios from 'axios';
 
-async function getResults(q) {
-    try {
-        const res = await axios(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=${q}&APPID=48e4bbf01e7d9297f784d4b9474efe79`)
-        console.log(res);
+export default class Search {
+    constructor(query) {
+        this.query = query
     }
-    catch (erorr){
-        alert(error)
-    }
-};
+    async getResults() {
+        try {
+            const res = await axios(`${proxy}api.openweathermap.org/data/2.5/weather?q=${this.query}&APPID=${key}`)
+            console.log(res);
+        }
+        catch (erorr){
+            alert(error)
+        }
+        getResults('amman');
+    };
+}
 
-getResults('amman');
+
